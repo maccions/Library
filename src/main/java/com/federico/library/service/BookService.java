9d3cbc6fa6dd0995e9d4f2f458def749ce4eb1a3
Maccions <f.maccioni.90@gmail.com>
2021-05-21 10:04:57 +0200

    removed javadoc

--- src/main/java/com/federico/library/service/BookService.java
+++ src/main/java/com/federico/library/service/BookService.java
@@ -8,86 +8,12 @@ import com.federico.library.exception.BookException;
 
 public interface BookService {
 
-	/**
-	 * Create a new book book by the given one, only need book title and the author
-	 * id. Throw BookException for missing book title or title already exist. Throw
-	 * authorException if no author found with given id.
-	 * 
-	 * @param book to be created
-	 * @return book created
-	 * @throws BookException   for invalid title
-	 * @throws AuthorException for invalid author
-	 */
 	public Book addBook(Book book) throws BookException, AuthorException;
-
-	/**
-	 * Remove the book given, only book id is needed. Throw BookException for
-	 * invalid id.
-	 * 
-	 * @param book to be removed
-	 * @return true
-	 * @throws BookException for invalid book.
-	 */
 	public boolean removeBook(Book book) throws BookException;
-
-	/**
-	 * Retrieve all book.
-	 * 
-	 * @return list of book
-	 */
 	public List<Book> getAllBook();
-
-	/**
-	 * Retrieves book with the given id or null if no book is found. Throw
-	 * BookException for invalid book id.
-	 * 
-	 * @param id to looking for
-	 * @return book or null
-	 * @throws BookException
-	 */
 	public Book getBookById(Long id) throws BookException;
-
-	/**
-	 * Retrieves book given or null if no book is found, only book id is needed.
-	 * Throw BookException for missing book or invalid book id.
-	 * 
-	 * @param book to looking for
-	 * @return book or null
-	 * @throws BookException
-	 */
 	public Book getBook(Book book) throws BookException;
-
-	/**
-	 * Retrieves the book with the given title or null if no book is find. Throw
-	 * BookException for missing parameter or invalid title.
-	 * 
-	 * @param book to search for
-	 * @return book or null
-	 * @throws BookException for invalid book or title
-	 */
 	public Book getBookByTitle(Book book) throws BookException;
-
-	/**
-	 * Retrieves all book write by the author given, only author id needed into book. Throw AuthorException for invalid author.
-	 * 
-	 * @param athor to search for
-	 * @return all book write by the given author
-	 * @throws AuthorException for invalid author
-	 * @throws BookException for missing book or author
-	 */
 	public List<Book> getAllBookByAuthor(Book book) throws AuthorException, BookException;
-
-	/**
-	 * Edit the given book, the book to edit must have an id. If there isn't some
-	 * attribute let it unchanged. To change the author only the new author id is
-	 * required. Throw BookException if there isn't book in repository or a book
-	 * with the same title already exist. Throw AuthorException if no author is
-	 * found with that id.
-	 * 
-	 * @param book to edit
-	 * @return book edited
-	 * @throws BookException   if no original book found or duplicated title
-	 * @throws AuthorException if no author is found
-	 */
 	public Book editBook(Book book) throws BookException, AuthorException;
 }
--- src/main/java/com/federico/library/service/BookShelfService.java
+++ src/main/java/com/federico/library/service/BookShelfService.java
@@ -7,61 +7,11 @@ import com.federico.library.exception.BookShelfException;
 
 public interface BookShelfService {
 
-	/**
-	 * Add given book shelf to repository. throw BookShelfException for missing book
-	 * shelf data or book shelf with same data exist. ignore any given id.
-	 * 
-	 * @param bookShelf to add
-	 * @return bookshelf created
-	 * @throws BookShelfException for invalid parameter
-	 */
 	public BookShelf addBookShelf(BookShelf bookShelf) throws BookShelfException;
-
-	/**
-	 * Search bookShelf shelf with the given id, return target book shelf or null.
-	 * throw BookShelfException for null id
-	 * 
-	 * @param bookShelfId to looking for
-	 * @return bookShelf or null
-	 * @throws BookShelfException for invalid or missing parameter
-	 */
 	public BookShelf getBookShelfById(Long bookShelfId) throws BookShelfException;
-	
-	/**
-	 * find a book shelf in repository, only book shelf id is needed. return target book shelf
-	 * or null. throw BookShelfException for missing bookShelf or missing id.
-	 * 
-	 * @param bookShelf to looking for
-	 * @return bookShelf or null
-	 * @throws BookShelfException for missing or invalid parameter
-	 */
 	public BookShelf getBookShelf(BookShelf bookShelf) throws BookShelfException;
-	
-	/**
-	 * get all bookshelf
-	 * 
-	 * @return list of bookshelf
-	 */
 	public List<BookShelf> getAllBookShelf();
-
-	/**
-	 * Edit a book shelf with the given data, update only data found in given book
-	 * shelf. throw BookShelfException for missing or invalid parameter given.
-	 * 
-	 * @param bookShelf to be edited
-	 * @return bookshelf edited
-	 * @throws BookShelfException for invalid or missing parameter
-	 */
 	public BookShelf editBookShelf(BookShelf bookShelf) throws BookShelfException;
-	
-	/**
-	 * Remove book shelf given, only book shelf Id is needed. Throw
-	 * BookShelfException for invalid id or it book shelf isn't empty.
-	 * 
-	 * @param bookShelf to be removed
-	 * @return true
-	 * @throws BookShelfException for invalid parameters or not empty bookshelf
-	 */
 	public boolean removeBookShelf(BookShelf bookShelf) throws BookShelfException;
 
 }
